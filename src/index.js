d7fbcbe3c51a7ee2a7adbe25e07b460123d4784b
@Github3636 <qudusnafiu4@gmail.com>
2024-02-12 06:38:37 +0000

    setting update

--- src/index.js
+++ src/index.js
@@ -29,17 +29,17 @@ window.addEventListener('beforeinstallprompt', (e) => {
 // Call this function on your 'Install' button event handler
 const handleInstallClick = () => {
   // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
+        console.log('User accepted the install prompt');
       } else {
-        console.log('User dismissed the A2HS prompt');
+        console.log('User dismissed the install prompt');
       }
       deferredPrompt = null;
     });
+  }
 };
 
 // Make the function available globally to call it from anywhere, e.g. from a button's onClick event
